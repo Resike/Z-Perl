b290545958834f3b9ee36e1aad5a9bb50f784c56
Fieora <anderson.kevin.c@gmail.com>
2023-11-28 21:20:49 -0600

    Fix target prediction bug

--- ZPerl_Target/ZPerl_Target.lua
+++ ZPerl_Target/ZPerl_Target.lua
@@ -962,7 +962,7 @@ end
 
 -- XPerl_Target_UpdateHotsPrediction
 local function XPerl_Target_UpdateHotsPrediction(self)
-	if IsClassic then
+	if not IsClassic then
 		return
 	end
 	if self == XPerl_Target then
